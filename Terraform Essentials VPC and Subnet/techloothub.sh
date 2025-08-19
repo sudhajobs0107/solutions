82a4708ce0cbec0e585835c8c642183be8ff7ae2
Sudha Yadav <sudhajobs0107@gmail.com>
2025-08-19 21:04:29 +0530

    Create techloothub.sh

---
+++ Terraform Essentials VPC and Subnet/techloothub.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Print the welcome message
+echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}         INITIATING EXECUTION...  ${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+
+# Fetch zone and region
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
+
+gcloud config set project $PROJECT_ID
+
+gcloud config set compute/region $REGION
+
+gcloud config set compute/zone $ZONE
+
+
+gcloud storage buckets create gs://$PROJECT_ID-terraform-state --project=$PROJECT_ID --location=us
+gcloud services enable cloudresourcemanager.googleapis.com --project=$PROJECT_ID
+
+gsutil versioning set on gs://$PROJECT_ID-tf-state
+
+mkdir terraform-firewall && cd $_
+
+
+cat > main.tf <<EOF_END
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "$PROJECT_ID-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = "$PROJECT_ID"
+  region  = "$REGION"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "custom-vpc-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet_us" {
+  name            = "subnet-us"
+  ip_cidr_range   = "10.10.1.0/24"
+  region          = "$REGION"
+  network         = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow_icmp" {
+  name    = "allow-icmp"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+EOF_END
+
+
+
+cat > variables.tf <<EOF_END
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project"
+  default     = "$PROJECT_ID"
+}
+
+variable "region" {
+  type        = string
+  description = "The region to deploy resources in"
+  default     = "$REGION"
+}
+EOF_END
+
+
+
+cat > outputs.tf <<EOF_END
+output "network_name" {
+  value       = google_compute_network.vpc_network.name
+  description = "The name of the VPC network"
+}
+
+output "subnet_name" {
+  value       = google_compute_subnetwork.subnet_us.name
+  description = "The name of the subnetwork"
+}
+EOF_END
+
+
+terraform init
+
+terraform plan
+
+terraform apply --auto-approve
+
+# Completion Message
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe my Channel (TechLootHub):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@techloothub${RESET_FORMAT}"
+
+
