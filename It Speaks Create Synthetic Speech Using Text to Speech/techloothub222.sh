3915cfe3c28e893cc0bc79ee2d3bd91eb8009680
Sudha Yadav <sudhajobs0107@gmail.com>
2025-08-21 22:26:27 +0530

    Create techloothub222.sh

---
+++ It Speaks Create Synthetic Speech Using Text to Speech/techloothub222.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Clear the screen
+clear
+
+
+# Print the welcome message
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||     Welcome to ${BOLD_TEXT}TechLootHub${RESET_FORMAT}${YELLOW_TEXT} Tutorials          ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||        ████████╗ ██╗      ██╗  ██╗            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██╔══╝ ██║      ██║  ██║            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ██║      ███████║            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ██║      ██╔══██║            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ███████╗ ██║  ██║            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ╚═╝    ╚══════╝ ╚═╝  ╚═╝            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||      Subscribe to our YouTube Channel :-      ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||      ${CYAN_TEXT}https://www.youtube.com/@techloothub${RESET_FORMAT}${YELLOW_TEXT}     ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ${BOLD_TEXT}INITIATING EXECUTION...${RESET_FORMAT}${YELLOW_TEXT}             ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo
+
+# Instruction for setting project ID
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Fetching the current project ID...${RESET_FORMAT}"
+export PROJECT_ID=$(gcloud config get-value project)
+echo $PROJECT_ID
+gcloud auth list
+export PROJECT_ID=$(gcloud config get-value project)
+gcloud services enable texttospeech.googleapis.com
+sudo apt-get install -y virtualenv
+python3 -m venv venv
+source venv/bin/activate
+gcloud iam service-accounts create tts-qwiklab
+gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwiklab@$PROJECT_ID.iam.gserviceaccount.com
+export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
+
+# Completion Message
+echo
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}||             ${BOLD_TEXT}LAB COMPLETED SUCCESSFULLY!           ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||     Thank you for using ${BOLD_TEXT}TechLootHub${RESET_FORMAT}${GREEN_TEXT} Tutorials     ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||  For more tutorials, visit our YouTube Channel:-  ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||       ${CYAN_TEXT}https://www.youtube.com/@techloothub${RESET_FORMAT}${GREEN_TEXT}        ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||      Don't forget to ${RED_TEXT}${BOLD_TEXT}LIKE,${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SHARE${RESET_FORMAT} ${GREEN_TEXT}and${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SUBSCRIBE${RESET_FORMAT}${GREEN_TEXT}    ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo ""
