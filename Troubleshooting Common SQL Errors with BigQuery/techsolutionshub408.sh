e8c99a838cf8bf3a82518eed53093d66bfacc5f0
Sudha Yadav <sudhajobs0107@gmail.com>
2025-08-23 00:03:38 +0530

    Create techsolutionshub408.sh

---
+++ Troubleshooting Common SQL Errors with BigQuery/techsolutionshub408.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Clear the screen
+clear
+
+
+# Print the welcome message
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||     Welcome to ${BOLD_TEXT}TechSolutionsHub${RESET_FORMAT}${YELLOW_TEXT} Tutorials     ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||        ████████╗ ████████╗ ██╗  ██╗           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██╔══╝ ██╔═════╝ ██║  ██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ████████╗ ███████║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ╚═════██║ ██╔══██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ████████║ ██║  ██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ╚═╝    ╚═══════╝ ╚═╝  ╚═╝           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||      Subscribe to our YouTube Channel :-      ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||  ${CYAN_TEXT}https://www.youtube.com/@techsolutionshub01${RESET_FORMAT}${YELLOW_TEXT}  ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ${BOLD_TEXT}INITIATING EXECUTION...${RESET_FORMAT}${YELLOW_TEXT}             ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo
+
+# Instruction for setting project ID
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Fetching the current project ID...${RESET_FORMAT}"
+export PROJECT_ID=$(gcloud config get-value project)
+echo $PROJECT_ID
+
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+fullVisitorId
+FROM `data-to-insights.ecommerce.rev_transactions`'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT fullVisitorId hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+fullVisitorId
+, hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+fullVisitorId
+, hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+COUNT(DISTINCT fullVisitorId) AS visitor_count
+, hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions`
+GROUP BY hits_page_pageTitle'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+COUNT(DISTINCT fullVisitorId) AS visitor_count
+, hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions`
+WHERE hits_page_pageTitle = "Checkout Confirmation"
+GROUP BY hits_page_pageTitle'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+geoNetwork_city,
+SUM(totals_transactions) AS totals_transactions,
+COUNT( DISTINCT fullVisitorId) AS distinct_visitors
+FROM
+`data-to-insights.ecommerce.rev_transactions`
+GROUP BY geoNetwork_city'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+geoNetwork_city,
+SUM(totals_transactions) AS totals_transactions,
+COUNT( DISTINCT fullVisitorId) AS distinct_visitors
+FROM
+`data-to-insights.ecommerce.rev_transactions`
+GROUP BY geoNetwork_city
+ORDER BY distinct_visitors DESC'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+geoNetwork_city,
+SUM(totals_transactions) AS total_products_ordered,
+COUNT( DISTINCT fullVisitorId) AS distinct_visitors,
+SUM(totals_transactions) / COUNT( DISTINCT fullVisitorId) AS avg_products_ordered
+FROM
+`data-to-insights.ecommerce.rev_transactions`
+GROUP BY geoNetwork_city
+ORDER BY avg_products_ordered DESC'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+geoNetwork_city,
+SUM(totals_transactions) AS total_products_ordered,
+COUNT( DISTINCT fullVisitorId) AS distinct_visitors,
+SUM(totals_transactions) / COUNT( DISTINCT fullVisitorId) AS avg_products_ordered
+FROM
+`data-to-insights.ecommerce.rev_transactions`
+GROUP BY geoNetwork_city
+HAVING avg_products_ordered > 20
+ORDER BY avg_products_ordered DESC'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+COUNT(DISTINCT hits_product_v2ProductName) as number_of_products,
+hits_product_v2ProductCategory
+FROM `data-to-insights.ecommerce.rev_transactions`
+WHERE hits_product_v2ProductName IS NOT NULL
+GROUP BY hits_product_v2ProductCategory
+ORDER BY number_of_products DESC
+LIMIT 5'
+
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT hits_product_v2ProductName, hits_product_v2ProductCategory
+FROM \`data-to-insights.ecommerce.rev_transactions\`
+GROUP BY 1,2
+"
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+COUNT(hits_product_v2ProductName) as number_of_products,
+hits_product_v2ProductCategory
+FROM \`data-to-insights.ecommerce.rev_transactions\`
+WHERE hits_product_v2ProductName IS NOT NULL
+GROUP BY hits_product_v2ProductCategory
+ORDER BY number_of_products DESC
+"
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+COUNT(DISTINCT hits_product_v2ProductName) as number_of_products,
+hits_product_v2ProductCategory
+FROM \`data-to-insights.ecommerce.rev_transactions\`
+WHERE hits_product_v2ProductName IS NOT NULL
+GROUP BY hits_product_v2ProductCategory
+ORDER BY number_of_products DESC
+LIMIT 5
+"
+
+# Completion Message
+echo
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}||             ${BOLD_TEXT}LAB COMPLETED SUCCESSFULLY!           ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||   Thank you for using ${BOLD_TEXT}TechSolutionsHub${RESET_FORMAT}${GREEN_TEXT} Tutorials  ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||  For more tutorials, visit our YouTube Channel:-  ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||    ${CYAN_TEXT}https://www.youtube.com/@techsolutionshub01${RESET_FORMAT}${GREEN_TEXT}    ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||      Don't forget to ${RED_TEXT}${BOLD_TEXT}LIKE,${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SHARE${RESET_FORMAT} ${GREEN_TEXT}and${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SUBSCRIBE${RESET_FORMAT}${GREEN_TEXT}    ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo ""
