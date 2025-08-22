44c76e72b3c5f1c5edc99fce853d1a055969d618
Sudha Yadav <sudhajobs0107@gmail.com>
2025-08-22 13:36:21 +0530

    Create techloothub277.sh

---
+++ APIs Explorer Qwik Start/techloothub277.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Clear the screen
+clear
+
+
+# Print the welcome message
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||     Welcome to ${BOLD_TEXT}TechLootHub${RESET_FORMAT}${YELLOW_TEXT} Tutorials          ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||        ████████╗ ██╗      ██╗  ██╗            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██╔══╝ ██║      ██║  ██║            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ██║      ███████║            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ██║      ██╔══██║            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ███████╗ ██║  ██║            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ╚═╝    ╚══════╝ ╚═╝  ╚═╝            ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||      Subscribe to our YouTube Channel :-      ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||      ${CYAN_TEXT}https://www.youtube.com/@techloothub${RESET_FORMAT}${YELLOW_TEXT}     ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ${BOLD_TEXT}INITIATING EXECUTION...${RESET_FORMAT}${YELLOW_TEXT}             ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo
+
+# Instruction for setting project ID
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Fetching the current project ID...${RESET_FORMAT}"
+export PROJECT_ID=$(gcloud config get-value project)
+echo $PROJECT_ID
+gcloud auth list
+gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket
+gsutil bucketpolicyonly set off gs://$DEVSHELL_PROJECT_ID-bucket
+gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-bucket
+wget https://github.com/quiccklabs/Labs_solutions/blob/master/Use%20APIs%20to%20Work%20with%20Cloud%20Storage%20Challenge%20Lab/world.jpeg
+mv world.jpeg demo-image.jpg
+gsutil cp demo-image.jpg  gs://$DEVSHELL_PROJECT_ID-bucket
+gsutil acl ch -u allUsers:R gs://$DEVSHELL_PROJECT_ID-bucket/demo-image.jpg
+# Completion Message
+echo
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}||             ${BOLD_TEXT}LAB COMPLETED SUCCESSFULLY!           ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||     Thank you for using ${BOLD_TEXT}TechLootHub${RESET_FORMAT}${GREEN_TEXT} Tutorials     ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||  For more tutorials, visit our YouTube Channel:-  ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||       ${CYAN_TEXT}https://www.youtube.com/@techloothub${RESET_FORMAT}${GREEN_TEXT}        ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||      Don't forget to ${RED_TEXT}${BOLD_TEXT}LIKE,${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SHARE${RESET_FORMAT} ${GREEN_TEXT}and${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SUBSCRIBE${RESET_FORMAT}${GREEN_TEXT}    ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo ""
