ca073a9316fc91e63286eeed6dbd9c9a47d4041c
Sudha Yadav <sudhajobs0107@gmail.com>
2025-09-04 18:13:38 +0530

    Create techsolutionshub123.sh

---
+++ Introduction to Cloud Dataproc Hadoop and Spark on Google Cloud/techsolutionshub123.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+BG_MAGENTA=$'\033[45m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Clear the screen
+clear
+
+
+# Print the welcome message
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||     Welcome to ${BOLD_TEXT}TechSolutionsHub${RESET_FORMAT}${YELLOW_TEXT} Tutorials     ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||        ████████╗ ████████╗ ██╗  ██╗           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██╔══╝ ██╔═════╝ ██║  ██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ████████╗ ███████║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ╚═════██║ ██╔══██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ████████║ ██║  ██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ╚═╝    ╚═══════╝ ╚═╝  ╚═╝           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||      Subscribe to our YouTube Channel :-      ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||  ${CYAN_TEXT}https://www.youtube.com/@techsolutionshub01${RESET_FORMAT}${YELLOW_TEXT}  ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ${BOLD_TEXT}INITIATING EXECUTION...${RESET_FORMAT}${YELLOW_TEXT}             ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo
+
+# Instruction for setting project ID
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Fetching the current project ID...${RESET_FORMAT}"
+PROJECT_ID=$(gcloud config get-value project 2> /dev/null)
+if [[ "$PROJECT_ID" == "(unset)" || -z "$PROJECT_ID" ]]; then
+  PROJECT_ID=$(gcloud projects list --format="value(projectId)" --limit=1)
+  gcloud config set project $PROJECT_ID
+fi
+export PROJECT_ID
+echo "${BG_MAGENTA}${BOLD_TEXT}Project ID is:-${RESET_FORMAT} $PROJECT_ID"
+
+gcloud auth list
+
+read -p "${BG_MAGENTA}${BOLD_TEXT}Enter the CLUSTER_NAME:-${RESET_FORMAT} " CLUSTER_NAME
+export CLUSTER_NAME
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+
+PROJECT_NUMBER=$(gcloud projects describe $(gcloud config get-value project) --format="value(projectNumber)")
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
+    --role="roles/storage.admin"
+
+sleep 60
+
+#!/bin/bash
+
+echo "$CLUSTER_NAME"
+echo "$ZONE"
+echo "$REGION"
+
+cluster_function() {
+  gcloud dataproc clusters create "$CLUSTER_NAME" \
+    --region "$REGION" \
+    --zone "$ZONE" \
+    --master-machine-type n1-standard-2 \
+    --worker-machine-type n1-standard-2 \
+    --num-workers 2 \
+    --worker-boot-disk-size 100 \
+    --worker-boot-disk-type pd-standard \
+    --no-address
+}
+
+cp_success=false
+
+while [ "$cp_success" = false ]; do
+  cluster_function
+  exit_status=$?
+
+  if [ "$exit_status" -eq 0 ]; then
+    echo "Function deployed successfully [https://www.youtube.com/@techsolutionshub01]"
+    cp_success=true
+  else
+    echo "Cluster creation failed. Checking if cluster already exists..."
+
+    if gcloud dataproc clusters describe "$CLUSTER_NAME" --region "$REGION" &>/dev/null; then
+      echo "Cluster already exists. Deleting. [https://www.youtube.com/@techsolutionshub01]"
+      gcloud dataproc clusters delete "$CLUSTER_NAME" --region "$REGION" --quiet
+      echo "Cluster deleted. Retrying in 10 seconds..."
+    else
+      echo "Unknown errors. Retrying in 10 seconds[https://www.youtube.com/@techsolutionshub01]"
+    fi
+    echo "Please subscribe to TechSolutionsHub [https://www.youtube.com/@techsolutionshub01]"
+    sleep 10
+  fi
+done
+
+
+gcloud dataproc jobs submit spark \
+    --project $DEVSHELL_PROJECT_ID \
+    --region $REGION \
+    --cluster $CLUSTER_NAME \
+    --class org.apache.spark.examples.SparkPi \
+    --jars file:///usr/lib/spark/examples/jars/spark-examples.jar \
+    -- 1000
+
+
+echo "Click this link to open" "${YELLOW}${BOLD}https://console.cloud.google.com/dataproc/jobs?project=$DEVSHELL_PROJECT_ID${RESET}"
+
+
+# Completion Message
+echo
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}||             ${BOLD_TEXT}LAB COMPLETED SUCCESSFULLY!           ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||   Thank you for using ${BOLD_TEXT}TechSolutionsHub${RESET_FORMAT}${GREEN_TEXT} Tutorials  ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||  For more tutorials, visit our YouTube Channel:-  ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||    ${CYAN_TEXT}https://www.youtube.com/@techsolutionshub01${RESET_FORMAT}${GREEN_TEXT}    ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||      Don't forget to ${RED_TEXT}${BOLD_TEXT}LIKE,${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SHARE${RESET_FORMAT} ${GREEN_TEXT}and${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SUBSCRIBE${RESET_FORMAT}${GREEN_TEXT}    ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo ""
