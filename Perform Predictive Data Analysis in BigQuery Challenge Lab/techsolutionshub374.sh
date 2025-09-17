9e117a1e63e72cb7f1d7bc720ff2208e1ffd188b
Sudha Yadav <sudhajobs0107@gmail.com>
2025-09-17 20:18:49 +0530

    Create techsolutionshub374.sh

---
+++ Perform Predictive Data Analysis in BigQuery Challenge Lab/techsolutionshub374.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+BG_MAGENTA=$'\033[45m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Clear the screen
+clear
+
+
+# Print the welcome message
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||     Welcome to ${BOLD_TEXT}TechSolutionsHub${RESET_FORMAT}${YELLOW_TEXT} Tutorials     ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||        ████████╗ ████████╗ ██╗  ██╗           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██╔══╝ ██╔═════╝ ██║  ██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ████████╗ ███████║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ╚═════██║ ██╔══██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ██║    ████████║ ██║  ██║           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ╚═╝    ╚═══════╝ ╚═╝  ╚═╝           ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||      Subscribe to our YouTube Channel :-      ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||  ${CYAN_TEXT}https://www.youtube.com/@techsolutionshub01${RESET_FORMAT}${YELLOW_TEXT}  ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||                                               ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}||           ${BOLD_TEXT}INITIATING EXECUTION...${RESET_FORMAT}${YELLOW_TEXT}             ||${RESET_FORMAT}"
+echo "${YELLOW_TEXT}===================================================${RESET_FORMAT}"
+echo
+
+# Instruction for setting project ID
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Fetching the current project ID...${RESET_FORMAT}"
+PROJECT_ID=$(gcloud config get-value project 2> /dev/null)
+if [[ "$PROJECT_ID" == "(unset)" || -z "$PROJECT_ID" ]]; then
+  PROJECT_ID=$(gcloud projects list --format="value(projectId)" --limit=1)
+  gcloud config set project $PROJECT_ID
+fi
+export PROJECT_ID
+echo "🧩 ${BG_MAGENTA}${BOLD_TEXT}Project ID is:-${RESET_FORMAT} $PROJECT_ID"
+
+gcloud auth list
+
+# --- Prompt for Required Inputs ---
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter event table name:-${RESET_FORMAT} " EVENT
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter table name:-${RESET_FORMAT} " TABLE
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter model name:-${RESET_FORMAT} " MODEL
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter value for VALUE_X1:-${RESET_FORMAT} " VALUE_X1
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter value for VALUE_Y1:-${RESET_FORMAT} " VALUE_Y1
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter value for VALUE_X2:-${RESET_FORMAT} " VALUE_X2
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter value for VALUE_Y2:-${RESET_FORMAT} " VALUE_Y2
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter UDF function name for FUNC_1:-${RESET_FORMAT} " FUNC_1
+read -rp "${BG_MAGENTA}${BOLD_TEXT}Enter UDF function name for FUNC_2:-${RESET_FORMAT} " FUNC_2
+
+
+
+bq load --source_format=NEWLINE_DELIMITED_JSON --autodetect $PROJECT_ID:soccer.$EVENT gs://spls/bq-soccer-analytics/events.json
+bq load --source_format=CSV --autodetect $PROJECT_ID:soccer.$TABLE gs://spls/bq-soccer-analytics/tags2name.csv
+
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+playerId,
+(Players.firstName || ' ' || Players.lastName) AS playerName,
+COUNT(id) AS numPKAtt,
+SUM(IF(101 IN UNNEST(tags.id), 1, 0)) AS numPKGoals,
+SAFE_DIVIDE(
+SUM(IF(101 IN UNNEST(tags.id), 1, 0)),
+COUNT(id)
+) AS PKSuccessRate
+FROM
+\`soccer.$EVENT\` Events
+LEFT JOIN
+\`soccer.players\` Players ON
+Events.playerId = Players.wyId
+WHERE
+eventName = 'Free Kick' AND
+subEventName = 'Penalty'
+GROUP BY
+playerId, playerName
+HAVING
+numPkAtt >= 5
+ORDER BY
+PKSuccessRate DESC, numPKAtt DESC
+"
+
+bq query --use_legacy_sql=false \
+"
+WITH
+Shots AS
+(
+SELECT
+*,
+/* 101 is known Tag for 'goals' from goals table */
+(101 IN UNNEST(tags.id)) AS isGoal,
+/* Translate 0-100 (x,y) coordinate-based distances to absolute positions
+using "average" field dimensions of 105x68 before combining in 2D dist calc */
+SQRT(
+POW(
+  (100 - positions[ORDINAL(1)].x) * $VALUE_X1/$VALUE_Y1,
+  2) +
+POW(
+  (60 - positions[ORDINAL(1)].y) * $VALUE_X2/$VALUE_Y2,
+  2)
+ ) AS shotDistance
+FROM
+\`soccer.$EVENT\`
+WHERE
+/* Includes both "open play" & free kick shots (including penalties) */
+eventName = 'Shot' OR
+(eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+)
+SELECT
+ROUND(shotDistance, 0) AS ShotDistRound0,
+COUNT(*) AS numShots,
+SUM(IF(isGoal, 1, 0)) AS numGoals,
+AVG(IF(isGoal, 1, 0)) AS goalPct
+FROM
+Shots
+WHERE
+shotDistance <= 50
+GROUP BY
+ShotDistRound0
+ORDER BY
+ShotDistRound0
+"
+
+echo "**********************************************************************************"
+echo "Please create the function, And then press Enter to continue..."
+echo "**********************************************************************************"
+
+read
+
+
+bq query --use_legacy_sql=false \
+"
+CREATE MODEL \`$MODEL\`
+OPTIONS(
+model_type = 'LOGISTIC_REG',
+input_label_cols = ['isGoal']
+) AS
+SELECT
+Events.subEventName AS shotType,
+/* 101 is known Tag for 'goals' from goals table */
+(101 IN UNNEST(Events.tags.id)) AS isGoal,
+\`$FUNC_1\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) AS shotDistance,
+\`$FUNC_2\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) AS shotAngle
+FROM
+\`soccer.$EVENT\` Events
+LEFT JOIN
+\`soccer.matches\` Matches ON
+Events.matchId = Matches.wyId
+LEFT JOIN
+\`soccer.competitions\` Competitions ON
+Matches.competitionId = Competitions.wyId
+WHERE
+/* Filter out World Cup matches for model fitting purposes */
+Competitions.name != 'World Cup' AND
+/* Includes both "open play" & free kick shots (including penalties) */
+(
+eventName = 'Shot' OR
+(eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+) AND
+\`$FUNC_2\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) IS NOT NULL
+;
+"
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+predicted_isGoal_probs[ORDINAL(1)].prob AS predictedGoalProb,
+* EXCEPT (predicted_isGoal, predicted_isGoal_probs),
+FROM
+ML.PREDICT(
+MODEL \`$MODEL\`, 
+(
+ SELECT
+   Events.playerId,
+   (Players.firstName || ' ' || Players.lastName) AS playerName,
+   Teams.name AS teamName,
+   CAST(Matches.dateutc AS DATE) AS matchDate,
+   Matches.label AS match,
+ /* Convert match period and event seconds to minute of match */
+   CAST((CASE
+     WHEN Events.matchPeriod = '1H' THEN 0
+     WHEN Events.matchPeriod = '2H' THEN 45
+     WHEN Events.matchPeriod = 'E1' THEN 90
+     WHEN Events.matchPeriod = 'E2' THEN 105
+     ELSE 120
+     END) +
+     CEILING(Events.eventSec / 60) AS INT64)
+     AS matchMinute,
+   Events.subEventName AS shotType,
+   /* 101 is known Tag for 'goals' from goals table */
+   (101 IN UNNEST(Events.tags.id)) AS isGoal,
+ 
+   \`soccer.$FUNC_1\`(Events.positions[ORDINAL(1)].x,
+       Events.positions[ORDINAL(1)].y) AS shotDistance,
+   \`soccer.$FUNC_2\`(Events.positions[ORDINAL(1)].x,
+       Events.positions[ORDINAL(1)].y) AS shotAngle
+ FROM
+   \`soccer.$EVENT\` Events
+ LEFT JOIN
+   \`soccer.matches\` Matches ON
+       Events.matchId = Matches.wyId
+ LEFT JOIN
+   \`soccer.competitions\` Competitions ON
+       Matches.competitionId = Competitions.wyId
+ LEFT JOIN
+   \`soccer.players\` Players ON
+       Events.playerId = Players.wyId
+ LEFT JOIN
+   \`soccer.teams\` Teams ON
+       Events.teamId = Teams.wyId
+ WHERE
+   /* Look only at World Cup matches to apply model */
+   Competitions.name = 'World Cup' AND
+   /* Includes both "open play" & free kick shots (but not penalties) */
+   (
+     eventName = 'Shot' OR
+     (eventName = 'Free Kick' AND subEventName IN ('Free kick shot'))
+   ) AND
+   /* Filter only to goals scored */
+   (101 IN UNNEST(Events.tags.id))
+)
+)
+ORDER BY
+predictedgoalProb
+"
+
+# Completion Message
+echo
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}||             ${BOLD_TEXT}LAB COMPLETED SUCCESSFULLY!           ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||   Thank you for using ${BOLD_TEXT}TechSolutionsHub${RESET_FORMAT}${GREEN_TEXT} Tutorials  ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||  For more tutorials, visit our YouTube Channel:-  ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||    ${CYAN_TEXT}https://www.youtube.com/@techsolutionshub01${RESET_FORMAT}${GREEN_TEXT}    ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||                                                   ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}||      Don't forget to ${RED_TEXT}${BOLD_TEXT}LIKE,${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SHARE${RESET_FORMAT} ${GREEN_TEXT}and${RESET_FORMAT} ${RED_TEXT}${BOLD_TEXT}SUBSCRIBE${RESET_FORMAT}${GREEN_TEXT}    ||${RESET_FORMAT}"
+echo "${GREEN_TEXT}=======================================================${RESET_FORMAT}"
+echo ""
